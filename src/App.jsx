6a599dc2d77b5b57308ae1996cd42c093c0c14bd
hakimxiao <alhakimabusolihin@gmail.com>
2025-11-03 22:24:28 +0700

    - MEMBUAT NAVBAR -

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import NavBar from "./components/NavBar.jsx";
 import HeroSection from "./sections/HeroSection.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 
 const App = () => {
   return (
     <>
+      <NavBar />
       <HeroSection />
       <ShowcaseSection />
     </>
