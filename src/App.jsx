9dde2b90b8fdaa841960bbc012504c4955545b55
hakimxiao <alhakimabusolihin@gmail.com>
2025-11-07 16:31:29 +0700

    Tambahkan LogoSection dan FeatureCard ke dalam App, tambahkan dependensi baru postprocessing, serta perbaiki struktur file dengan mengimplementasikan komponen terkait.
    - Serta membuat section keren yaitu logo dan feature card.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import LogoSection from "./sections/LogoSection.jsx";
 import NavBar from "./components/NavBar.jsx";
 import HeroSection from "./sections/HeroSection.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
+import FeatureCard from "./sections/FeatureCard.jsx";
 
 const App = () => {
-  return (
-    <>
-      <NavBar />
-      <HeroSection />
-      <ShowcaseSection />
-    </>
-  );
+    return (
+        <>
+            <NavBar/>
+            <HeroSection/>
+            <ShowcaseSection/>
+            <LogoSection/>
+            <FeatureCard/>
+        </>
+    );
 };
 export default App;
